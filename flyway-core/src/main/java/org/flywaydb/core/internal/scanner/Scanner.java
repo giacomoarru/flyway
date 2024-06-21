d61a84eafa7435371b0a3b65f3ce2e82081b5463
Jack Arru <jack@x2x.cloud>
2024-06-21 21:39:22 +0200

    "Remove MongoDB support from Flyway"
    This commit removes MongoDB support from Flyway. The support was previously implemented through a number of classes and configuration files specifically designed for MongoDB integration, which are no longer necessary. This includes the MongoDB database, connection, and template classes, and their associated resources.

--- flyway-core/src/main/java/org/flywaydb/core/internal/scanner/Scanner.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/scanner/Scanner.java
@@ -70,7 +70,6 @@ public class Scanner<I> implements ResourceProvider, ClassProvider<I> {
         FileSystemScanner fileSystemScanner = new FileSystemScanner(stream, configuration);
 
         FeatureDetector detector = new FeatureDetector(classLoader);
-        long cloudMigrationCount = 0;
         for (Location location : configuration.getLocations()) {
             if (location.isFileSystem()) {
                 resources.addAll(fileSystemScanner.scanForResources(location));
@@ -92,7 +91,6 @@ public class Scanner<I> implements ResourceProvider, ClassProvider<I> {
                 if (detector.isAwsAvailable()) {
                     Collection<LoadableResource> awsResources = new AwsS3Scanner(encoding, throwOnMissingLocations).scanForResources(location);
                     resources.addAll(awsResources);
-                    cloudMigrationCount += awsResources.stream().filter(r -> r.getFilename().endsWith(".sql")).count();
                 } else {
                     LOG.error("Can't read location " + location + "; AWS SDK not found");
                 }
@@ -104,9 +102,7 @@ public class Scanner<I> implements ResourceProvider, ClassProvider<I> {
         }
 
 
-        if (cloudMigrationCount > 100L) {
-            throw new FlywayEditionUpgradeRequiredException(Tier.TEAMS, LicenseGuard.getTier(configuration), "Cloud locations with more than 100 migrations");
-        }
+
 
 
         for (LoadableResource resource : resources) {
